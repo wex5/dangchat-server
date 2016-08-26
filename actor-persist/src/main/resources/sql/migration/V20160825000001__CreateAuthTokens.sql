68696a83e99a6d97df8e42558067eb83759e387c
rdc-justep <song@justep.com>
2016-08-26 02:01:42 +0000

    将token字段类型修改为varchar(32)
    git-svn-id: http://192.168.1.20/svn/im/trunk/actor-server@566 0c17fa83-6788-ff4f-83ba-5dfcf0814e92

--- actor-persist/src/main/resources/sql/migration/V20160825000001__CreateAuthTokens.sql
+++ actor-persist/src/main/resources/sql/migration/V20160825000001__CreateAuthTokens.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "public"."auth_tokens" (
 "user_id" varchar(32) COLLATE "default" NOT NULL,
-"token" varchar(255) COLLATE "default" NOT NULL,
+"token" varchar(32) COLLATE "default" NOT NULL,
 "created_at" timestamp(6) NOT NULL,
 "attempts" int4 DEFAULT 0 NOT NULL,
 CONSTRAINT "auth_tokens_pkey" PRIMARY KEY ("user_id")
