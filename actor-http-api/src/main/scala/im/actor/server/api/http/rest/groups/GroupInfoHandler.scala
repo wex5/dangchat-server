2d50eceeb14e9e5551b4c0f2816bb5b444430b91
lining90567 <lining90567@sina.com>
2017-02-23 09:26:27 +0000

    添加GroupDTO RESTful handler
    git-svn-id: http://192.168.1.20/svn/im/trunk/actor-server@724 0c17fa83-6788-ff4f-83ba-5dfcf0814e92

---
+++ actor-http-api/src/main/scala/im/actor/server/api/http/rest/groups/GroupInfoHandler.scala
@@ -0,0 +1,68 @@
+package im.actor.server.api.http.rest.groups
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.StatusCodes.OK
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import im.actor.server.db.DbExtension
+import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
+import im.actor.server.api.http.HttpHandler
+import im.actor.server.api.http.json.JsonFormatters
+import im.actor.server.api.http.rest.util.SecurityManager
+import im.actor.server.model.GroupDTO
+import im.actor.server.persist.GroupDTORepo
+import im.actor.server.persist.GroupMemberDTORepo
+
+/**
+ * Created by User on 2017/2/23.
+ */
+private[http] final class GroupInfoHandler()(implicit system: ActorSystem) extends HttpHandler with PlayJsonSupport {
+
+  import JsonFormatters._
+
+  private lazy val db = DbExtension(system).db
+  private lazy val config = system.settings.config
+
+  override def routes: Route =
+    defaultVersion {
+      pathPrefix("dangchat-groupInfo") {
+        pathEnd {
+          post {
+            complete("The post http request is not processed!")
+          }
+        } ~
+          get {
+            //获取request对象 request =>
+            path(Segment) { groupId ⇒
+              //单个获取参数：ctx.uri.query.get("apiPwd")
+              parameters('apiPassword) { apiPassword ⇒
+                if (SecurityManager.checkApiPassword(apiPassword, config)) {
+                  //根据群组Id查询群组
+                  var optGroupDTO: Option[GroupDTO] = null
+                  optGroupDTO = scala.concurrent.Await.result(
+                    db.run(GroupDTORepo.findById(Integer.parseInt(groupId))), scala.concurrent.duration.Duration.Inf
+                  )
+                  optGroupDTO match {
+                    case Some(groupDTO) ⇒
+                      val groupMemberDTOs = scala.concurrent.Await.result(
+                        db.run(GroupMemberDTORepo.findByGroupId(Integer.parseInt(groupId))), scala.concurrent.duration.Duration.Inf
+                      )
+                      complete(OK → im.actor.server.api.http.json.GroupDTOInfo(groupDTO.groupId, groupDTO.groupName, groupMemberDTOs))
+                    case None ⇒ complete(OK → im.actor.server.api.http.json.GroupDTOInfo(0, "", null))
+                  }
+                } else {
+                  complete("Api password错误！")
+                }
+              }
+            }
+          } ~
+          put {
+            complete("The put http request is not processed!")
+          } ~
+          delete {
+            complete("The delete http request is not processed!")
+          }
+      }
+    }
+
+}
